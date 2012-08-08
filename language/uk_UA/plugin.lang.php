867dd3100421830cb6eeedab2dc3b60f02c93b6a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-08-08 22:33:36 +0000

    [register_fluxbb] Update uk_UA, thanks to : animan
    git-svn-id: http://piwigo.org/svn/extensions/Register_FluxBB@17482 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/uk_UA/plugin.lang.php
+++ language/uk_UA/plugin.lang.php
@@ -27,4 +27,17 @@ $lang['Title'] = 'Реєстрація FluxBB';
 $lang['error_config_guest'] = 'ПОМИЛКА : ім\'я облікового запису гостя FluxBB неправильне!';
 $lang['error_config_admin1'] = 'ПОМИЛКА : ім\'я користувача адміністратора Piwigo неправильне!';
 $lang['save_config'] = 'Налаштування збережені';
+$lang['New_Link'] = 'Пов\'язані облікові записи:';
+$lang['Prefix'] = 'Префікс таблиць FluxBB:';
+$lang['Error_Synchro_Mail'] = 'на адресу електронної пошти';
+$lang['Error_Synchro_Pswd'] = 'на паролі';
+$lang['Guest'] = 'Ім\'я користувача "Гостя" на FluxBB.';
+$lang['Link_Dead'] = 'Видалення мертвих посилань';
+$lang['Link_Del'] = 'Видалити посилання:';
+$lang['Link_Dup'] = 'Видалення дублікатів';
+$lang['Sync_Btn'] = 'Синхронізація';
+$lang['Sync_OK'] = 'Синхронізація OK<br><br>';
+$lang['Sync_User'] = 'Обліковий запис синхронізації';
+$lang['Tab_Info'] = 'Інструкції';
+$lang['Title_Tab'] = 'Register_FluxBB - Версія:';
 ?>
