c97682f86579958fc37eab37d43ba64174a43a58
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-09-28 04:43:10 +0000

    [register_fluxbb] Update da_DK, thanks to : Kaare
    git-svn-id: http://piwigo.org/svn/extensions/Register_FluxBB@18317 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/da_DK/plugin.lang.php
@@ -0,0 +1,30 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['About_Bridge'] = 'Om Register_FluxBB / UAM-forbindelse';
+$lang['About_Reg'] = 'Om registrering af brugere på FluxBB-forummet';
+$lang['Add_User'] = 'Tilføjelse af FluxBB-konto:';
+$lang['Admin'] = 'Piwigos administratorbrugernavn. <b style="color: red">Brugernavnet på FluxBB\'s administratorkonto skal være det samme!</b>';
+$lang['Advise_Check_Dup'] = '<b>Det er ikke muligt at fortsætte synkroniseringen, hvis du har duplikater i Piwigos eller FluxBB\'s brugerkontotabel.</b><br><br>';
+$lang['Advise_FluxBB_Dup'] = '<b>ADVARSEL! Du skal foretage disse rettelser i FluxBB før du fortsætter<br>benyt ikonerne til at slette brugere fra FluxBB og løse problemet.</b>';
+$lang['Advise_PWG_Dup'] = '<b>ADVARSEL! Du skal foretage disse rettelser i Piwigo før du kan fortsætter<br>benyt Piwigos brugerhåndtering til at løse problemet.</b>';
+?>
