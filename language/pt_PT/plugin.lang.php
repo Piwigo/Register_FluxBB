64bff93e5bafcea6c2e3c6a9ccab90299b013065
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-07-19 19:25:48 +0000

    [register_fluxbb] Add pt_PT, thanks to : ANO
    git-svn-id: http://piwigo.org/svn/extensions/Register_FluxBB@24002 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/pt_PT/plugin.lang.php
@@ -0,0 +1,89 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Details'] = 'Nível de detalhe a mostrar nos relatórios das operações (sincronização';
+$lang['error_config_admin1'] = 'ERRO: Nome de webmaster no Piwigo, está errado!';
+$lang['error_config_guest'] = 'ERRO: O nome de utilizador como visitante no FluxBB está errado!';
+$lang['save_config'] = 'Definições salvas';
+$lang['To synchronize your forum access with the gallery you have been registered at %s!'] = 'Para sincronizar o acesso ao forum com a galeria, deverá estar registado(a) em %s!';
+$lang['Sync_User'] = 'Sincronização da conta:';
+$lang['Sync_Title_d'] = 'Usar para sincronizar contas';
+$lang['Sync_Title'] = 'Sincronização de contas do Piwigo para FluxBB';
+$lang['Sync_PWG2FluxBB'] = '<b>Análise de contas existentes no Piwigo mas omitidas no FluxBB</b>';
+$lang['Error_Link_Dup'] = '<b>Erro na tabela de ligação, existem duplicados:</b> ';
+$lang['Sync_OK'] = 'Sincronização OK<br/><br/>';
+$lang['Prefix'] = 'Prefixo das tabelas FluxBB ';
+$lang['Error_Link_Del'] = '<b>Erro na tabela de ligação entre dois utilizadores:</b> ';
+$lang['Sync_Btn'] = 'Sincronização';
+$lang['Sync_Check_Dup'] = '<b>Análise da tabela de utilizadores para controlo de duplicados</b>';
+$lang['Instruction_Title'] = 'Instruções ( importante ler primeiro)';
+$lang['Instruction_Title_d'] = 'Instruções e informação importante';
+$lang['Link_Dead'] = 'Remoção de links mortos';
+$lang['Link_Del'] = 'Remoção de link';
+$lang['Link_Dup'] = 'Remoção de duplicados';
+$lang['New_Link'] = 'Conta ligada:';
+$lang['Del_Pt_true'] = ' --&gt; Apagar tudo';
+$lang['Del_User'] = 'Remoção de conta no FluxBB:';
+$lang['Error_FluxBB2PWG'] = '<b>Conta no FluxBB não no Piwigo:</b> ';
+$lang['Error_FluxBB_Dup'] = '<b>Erro na tabela contas FluxBB\'s, existem duplicados:</b> ';
+$lang['Error_Link_Break'] = '<b>Ligação quebrada entre as contas Piwigo e FluxBB :</b> ';
+$lang['Error_Link_Dead'] = '<b>Erro na tabela de ligações, links mortos:</b> ';
+$lang['FluxBB_Title'] = 'Register_FluxBB - Versão: ';
+$lang['Guest'] = 'Nome de visitante no FluxBB';
+$lang['Error_PWG2FluxBB'] = '<b>Na conta Piwigo e não no FluxBB:</b> ';
+$lang['Error_PWG_Dup'] = '<b>Erro na tabela de contas Piwigo, existem duplicados:</b> ';
+$lang['Error_Synchro'] = '<b>Sincronização incorreta da conta:</b> ';
+$lang['Error_Synchro_Mail'] = 'Nas direções de Email';
+$lang['Audit_Link_Bad'] = '<b>Auditar ligações incorretas entre Piwigo e contas FluxBB</b>';
+$lang['Audit_Link_Break'] = '<b>Auditar e reparar ligações entre contas Piwigo e FluxBB</b> ';
+$lang['Audit_OK'] = 'Auditoria OK<br/><br/>';
+$lang['Audit_PWG2FluxBB'] = '<b>Auditar contas existentes no Piwigo e omitidas noFluxBB</b>';
+$lang['Audit_PWG_Dup'] = '<b>Auditar tabela de contas Piwigo</b>';
+$lang['Audit_Synchro_OK'] = ' <b>: Sincronização de dados OK</b>';
+$lang['Bridge_UAM_false'] = ' --> Desativar ponte Register_FluxBB / UAM (defeito)';
+$lang['Bridge_UAM_true'] = ' --> Ativar ponte Register_FluxBB / UAM ';
+$lang['Config_Title'] = 'Instalar exteção';
+$lang['Config_Title1'] = 'Definições da ponte entre FluxBB e Piwigo';
+$lang['Config_Title2'] = 'Definições avançadas do Piiwigo';
+$lang['Config_Title_d'] = 'Instalar extenção';
+$lang['Confirm'] = 'Confirmar ações corretivas na auditoria';
+$lang['Confirm_false'] = ' --&gt; Requerida confirmação antes de qualquer ação na auditoria';
+$lang['Confirm_true'] = '--&gt; Não solicitar confirmação';
+$lang['Del_Pt'] = 'Apagar Utilizadores e mensagens do forum quando forem apagados no Piwigo';
+$lang['Del_Pt_false'] = '--&gt; Não apagar do forum tópicos e questões colocadas ';
+$lang['%d email addresses already exist: %s'] = '%d esta direção de email já existe: %s';
+$lang['%d email addresses rejected: %s'] = '%d direção de email regeitada: %s';
+$lang['%d registrations on error: %s'] = '%d registos com erro: %s';
+$lang['%d users registered'] = '%d utilizadores registados';
+$lang['About_Bridge_Title'] = 'Acerca de Register_FluxBB / UAM bridge';
+$lang['About_Bridge_Title_d'] = 'Intruções acerca da ponte entre Register_FluxBB e extenções UserAdvManager';
+$lang['About_Reg_Title'] = 'Acerca do registo como utilizador no forum FluxBB';
+$lang['About_Reg_Title_d'] = 'Instruções muito úteis para uma melhor integração';
+$lang['Add_User'] = 'Adicionando uma conta no FluxBB';
+$lang['Add_User2pwg'] = 'Adicionando uma conta no Piwigo';
+$lang['Advise_Check_Dup'] = '<b>IMPOSSIVEL continuar a sincronização se possui contas de utilizador duplicadas  ou FluxBB. Corrija s.F.F. e tente de novo.</b><br/><br/>';
+$lang['Advise_FluxBB_Dup'] = '<b>ATENÇÃO! Deverá efectuar estas correções no FluxBB before antes de continuar<br/>Utilizar os icons para apagar utilizadores do FluxBB e resolver o problema.</b> Advise_FluxBB_Dup  ';
+$lang['Advise_PWG_Dup'] = 'Deverá efectuar estas correções no PIWIGO antes de continuar <br/> Usar a administração de utilizadores Piwigo para resolver o  problema.</b>';
+$lang['Audit_Btn'] = 'Auditar';
+$lang['Audit_FluxBB2PWG'] = '<b>Auditar contas existentes no FluxBB e omitidas no Piwigo</b>';
+$lang['Audit_FluxBB_Dup'] = 'Auditar a tabela contas do FluxBB\'s ';
+?>
