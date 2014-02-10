78411be8d88cef8988ca26514e62096d22fd4b1a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-02-10 18:06:49 +0000

    [register_fluxbb] Update sk_SK, thanks to : JoeKundlak
    git-svn-id: http://piwigo.org/svn/extensions/Register_FluxBB@27310 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/sk_SK/plugin.lang.php
+++ language/sk_SK/plugin.lang.php
@@ -23,4 +23,21 @@
 $lang['error_config_admin1'] = 'CHYBA:  Užívateľské meno pre Piwigo "Administrátora" je nesprávne';
 $lang['error_config_guest'] = 'CHYBA : Užívateľské meno pre návštevnícky účet FluxBB (guest) nie je správny!';
 $lang['save_config'] = 'Nastavenie uložené';
+$lang['Config_Title'] = 'Nastavenia zásuvného modulu';
+$lang['Config_Title1'] = 'Nastavenia spojenia medzi FluxBB a Piwigo';
+$lang['Config_Title2'] = 'Pokročilé nastavenia zásuvného modulu';
+$lang['Config_Title_d'] = 'Nastavenie zásuvného modulu';
+$lang['Confirm'] = 'Potvrdenie opravných akcií v audite';
+$lang['%d email addresses already exist: %s'] = 'Už existuje %d emailových adries: %s';
+$lang['%d email addresses rejected: %s'] = '%d odmietnutých emailových adries: %s';
+$lang['%d registrations on error: %s'] = '%d chybných registrácií: %s';
+$lang['%d users registered'] = '%d registrovaných užívateľov';
+$lang['About_Bridge_Title'] = 'O "Register_FluxBB / UAM bridge"';
+$lang['About_Bridge_Title_d'] = 'Inštrukcie o spojení medzi zásuvnými modulmi Register_FluxBB a UserAdvManager';
+$lang['About_Reg_Title'] = 'O registrácii užívateľov na FluxBB fórum';
+$lang['About_Reg_Title_d'] = 'Užitočné informácie pre lepšiu integráciu';
+$lang['Add_User'] = 'Pridávam účet do FluxBB:';
+$lang['Add_User2pwg'] = 'Pridávam účet do Piwigo:';
+$lang['Audit_Btn'] = 'Audit';
+$lang['Audit_OK'] = 'Audit v poriadku<br/><br/>';
 ?>
