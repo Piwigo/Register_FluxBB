0c22566fa3c6ea063cdd243d9bfc734416cc3a67
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-10-03 06:28:19 +0000

    [register_fluxbb] Update nl_NL, thanks to : Blackbird
    git-svn-id: http://piwigo.org/svn/extensions/Register_FluxBB@29876 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/nl_NL/plugin.lang.php
+++ language/nl_NL/plugin.lang.php
@@ -88,3 +88,65 @@ $lang['%d users registered'] = '%d gebruikers geregistreerd';
 $lang['%d registrations on error: %s'] = '%d registratiefouten: %s';
 $lang['%d email addresses rejected: %s'] = '%d emailadres verworpen: %s';
 $lang['%d email addresses already exist: %s'] = '%d dit e-mailadres bestaat al: %s';
+$lang['UAM_Bridge_advice'] = 'De UserAdvManager plug-in staat toe om nieuw registrerende personen te forceren om hun registratie te bevestigen voordat ze toegang krijgen tot de gehele galerij. Het gezamenlijke gebruik van deze plug-in met Register_FluxBB kan hetzelfde doen op het aangesloten forum: registrerende bezoekers kunnen dan geen berichten plaatsen totdat zij hun registratie op de galerij hebben gevalideerd.<br />
+Hier volgt de algemene procedure om dit toe te passen:<br />
+- Configureer tenminste twee gebruikersgroepen in het beheerderspaneel van je FluxBB-forum (bijvoorbeeld: "gevalideerd" en "niet_gevalideerd").<br />
+- Geef de eerste groep ("gevalideerd") permissies die jij wilt op je forum en verkies dit als de standaard groep.<br />
+- Verwijder voor de tweede groep ("niet_gevalideerd") alle permissies op je forum (de leden van deze groep kunnen dan alleen publieke berichten bekijken).<br />
+- Localiseer de ID van de tweede groep "niet_gevalideerd".<br />
+- Vul deze ID in de Register_FluxBB plug-in in, activeer de overbrugging en sla de instellingen op.<br />
+<b class="mod"><u>Belangrijke notities:"</u></b><br />
+Als je al eens eerder een oudere versie van Register_FluxBB hebt gebruikt, dan zullen de overbruggingsinstellingen geen uitoefening hebben op de Piwigo-accounts die gelinkt zijn tussen je galerij en je FluxBB-forum. Alleen nieuwe registraties zullen worden aangepast door de activatie van de overbrugging. <b><u>De functie om de account opnieuw te synchroniseren moet leeg zijn.</u></b><br />
+Vergelijkbaar is dat, wanneer je nooit eerder Register_FluxBB hebt gebruikt, het migratieproces van de Piwigo-accounts van je galerij naar je FluxBB-forum de validatiestaat veronachtzaamt of niet voor de accounts bij het starten van de migratie.';
+$lang['save_config'] = 'Instellingen opgeslagen';
+$lang['error_config_guest'] = 'FOUT: De gebruikersnaam van de gastaccount in FluxBB is onjuist!';
+$lang['error_config_admin2'] = 'FOUT: De gebruikersnaam van de FluxBB administratoraccount is anders dan die van Piwigo! Controleer de configuratie van je FluxBB-forum en hernoem de administratoraccount naar dezelfde naam als die je hebt gebruikt in de Piwigo Galerij.';
+$lang['error_config_admin1'] = 'FOUT: Piwigo\'s "Webmaster"-gebruikersnaam is onjuist!';
+$lang['To synchronize your forum access with the gallery you have been registered at %s!'] = 'Je bent geregistreerd op %s. We moeten je forumtoegang synchroniseren met de galerij.';
+$lang['Sync_User'] = 'Accountsynchronisatie:';
+$lang['Sync_Title_d'] = 'Gebruik om accounts opnieuw te synchroniseren';
+$lang['Sync_Title'] = 'Synchroniseer accounts van Piwigo naar FluxBB';
+$lang['Sync_Text'] = '<div class="warning">Synchronisatie is een massa-actie die grote invloed kan hebben op je forumgebruikers! Controleer om elke zaak goed te kunnen beheren.</div><br /><br />
+
+Herinneringen:<br />
+De wachtwoorden van handmatig gesynchroniseerde accounts (via een controle of de globale synchronisatie) van Piwigo naar FluxBB worden niet hersteld. Elke gebruiker die hierover ongerust is zou zijn wachtwoord moeten wijzigen bij de volgende log-in op de galerij (hij zal dan automatisch worden doorgestuurd naar zijn profielpagina), zodat de synchronisatie effectief kan zijn en de persoon verbinding kan maken met het forum.<br /><br />
+Tot nu toe is het niet mogelijk om bestaande gebruikers op een FluxBB-forum te synchroniseren naar een Piwigo-galerij. Dit is de reden waarom de voorgestelde controle-actie FluxBB-accounts verwijdert. Je kan deze niet-gesynchroniseerde statische accounts laten voor wat ze zijn (deze gebruikers zullen alleen kunnen verbinden met het forum), wachtend voor de volgende evolutie van deze plug-in die ook deze synchronisatie kan uitvoeren.<br /><br />';
+$lang['Sync_PWG2FluxBB'] = '<b>Analyse van bestaande accounts in Piwigo die in FluxBB missen</b>';
+$lang['Sync_OK'] = 'Synchronisatie OK<br /><br />';
+$lang['Sync_Link_Break'] = '<b>Analyse van repareerbare links tussen accounts in Piwigo en FluxBB</b>';
+$lang['Sync_Link_Bad'] = '<b>Analyse van foutieve overeenkomsten tussen Piwigo- en FluxBB-accounts</b>';
+$lang['Sync_FluxBB2PWG'] = '<b>Analyse van bestaande accounts in FluxBB die in Piwigo missen</b>';
+$lang['Sync_DataUser'] = '<b>Analyse van wachtwoorden en emailadressen tussen Piwigo- en FluxBB-accounts</b>';
+$lang['Sync_Check_Dup'] = '<b>Analyse van gebruikerstabellen voor duplicaatcontrole</b>';
+$lang['Sync_Btn'] = 'Synchronisatie';
+$lang['RegFluxBB_Email_or_Username_already_exist'] = 'Synchronisatie van FluxBB naar Piwigo is gestopt: De doelemail of -gebruikersnaam bestaat reeds in de gebruikerstabel van Piwigo.';
+$lang['RegFluxBB_Password_Reset_Msg'] = 'Update alsjeblieft je wachtwoord voor synchronisatie met het forum. Dan zal je de mogelijkheid hebben om in te loggen op het forum met dezelfde account als op de galerij.';
+$lang['Prefix'] = 'FluxBB prefix tabellen:';
+$lang['Please change your password at your first connexion on the gallery'] = 'Verander alsjeblieft je wachtwoord bij je eerstvolgende bezoek aan de galerij om de datasynchronisatie te be&euml;indigen.';
+$lang['No_Reg_advise'] = 'Voor betere integratie is het adviseerbaar om de volgende aanpassingen aan je FluxBB-forum te maken (<b>Waarschuwing: deze veranderingen zullen teniet worden gedaan bij het updaten van het forum</b>):<br /><br />
+<b>* Verander de waarde bij "Sta nieuwe registraties toe" naar "NEE" in het beheerderspaneel van FluxBB.<br /><br />
+<b>* Pas het bestand</b>: [FluxBBRoot]/lang/English/register.php aan door de volgende regel aan te passen:<
+<div class="mod">\'No new regs\' => \'Dit forum accepteert geen nieuwe gebruikers.\'</div>. 
+<b>Pas dit aan naar:</b>
+<div class="info">\'No new regs\' => \'&lt;a href=&quot;http://[YourPiwigoRoot]/register.php&quot; &gt; Klik hier om te registreren &lt;/a&gt;&lt;br/&gt;&lt;br/&gt;\'</div><br />
+Natuurlijk moet je deze aanpassing ook doorvoeren in de andere talen die op je forum geactiveerd zijn.<br /><br />
+<b>*Pas het bestand</b> [FluxBBRoot]/login.php <b>aan door het volgende te vervangen op regel 64:</b>
+<div class="mod">message($lang_login[\'Wrong user/pass\'].\'&lt;a href=&quotlogin.php?action=forget&quot;&gt;</div> <b>door:</b>
+<div class="info">message($lang_login[\'Wrong user/pass\'].&lt;a href=&quot;../[YourPiwigoRoot]/password.php&quot;&gt;</div><br />
+<b>En op regel 295:</b>
+<div class="mod">&lt;a href=&quot;login.php?action=forget&quot;tabindex=&quot;5&quot;&gt;&lt;?php echo $lang_login[\'Forgotten pass\']&lt;/a&gt;</div> <b>door:</b>
+<div class="info">&lt;a href=&quot;../[YourPiwigoRoot]/password.php&quot;tabindex=&quot;5&quot;&gt;&lt;?php echo $lang_login[\'Forgotten pass\']?&gt;&lt;/a&gt;</div><br /><br />
+<b>* Pas het bestand</b> [FluxBBRoot]/index.php <b>aan ACHTER regel 18:</b>
+<div class="mod">
+//Load the index.php language file<br />
+require PUN_ROOT.\'lang/\'.$pun_user[\'language\'].\'index.php\';
+</div>
+<b>Voeg hieraan toe (erachter):</b>
+<div class="info">
+//Mod to regenerate user cache on each load<br />
+&nbsp;if (!defined(\'FORUM_CACHE_FUNCTIONS_LOADED\'))<br />
+&nbsp;&nbsp;&nbsp;require PUN_ROOT.\'include/cache.php\';
+<br /><br />
+&nbsp;&nbsp;&nbsp;generate_users_info_cache();<br />
+// ---------------------------<br />
+</div><br />';
